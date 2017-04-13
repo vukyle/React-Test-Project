bd9a9a28da5743da667fef0fbd2b79d69242b492
Vu Le <vu.ky.le@gmail.com>
2017-04-13 12:05:39 -0700

    Complete ReactJS Basics #5

---
+++ src/app/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class Header extends React.Component {
+    render() {
+        return(
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <ul className="nav navbar-header">
+                            <li><a href="#">Home</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
